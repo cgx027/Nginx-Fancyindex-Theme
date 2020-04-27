6b85569bbe73a983e5dee60a9b2452e8b7bc0a69
Ted Chen <ted.chen@emc.com>
2020-04-26 23:37:55 -0700

    add full path

--- js/history.js
+++ js/history.js
@@ -42,6 +42,7 @@ if (!!(window.history && history.pushState)) {
           breadcrumbs += '<a href="' + currentPath + '">Root<\/a>';
         }
       }
+        breadcrumbs += '<h3>' + loc + '</>'
       document.getElementById('breadcrumbs').innerHTML = breadcrumbs;
     }, 500);
   };
